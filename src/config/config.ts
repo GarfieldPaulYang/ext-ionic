1b6b2d6d404c1105e301e744dd6d4ff36fac0f54
am-wangjin <am-wangjin@gmail.com>
2017-02-14 08:53:16 +0800

    资源初始化不由登录提供

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,6 @@ import { ImageLoaderOptions } from '../components/image-loader/image-loader-opti
 import { BaiduMapOptions } from '../components/baidu-map/baidu-map-options';
 
 export interface LoginConfig {
-  appKey?: string;
   url?: string;
   ticket?: string;
 }
