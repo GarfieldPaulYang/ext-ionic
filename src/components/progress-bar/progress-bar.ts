6d169e73187ea0cf5435c57128cc4b1f9f167187
am-wangjin <am-wangjin@gmail.com>
2017-03-01 08:40:03 +0800

    修改名称

--- src/components/progress-bar/progress-bar.ts
+++ src/components/progress-bar/progress-bar.ts
@@ -1,16 +1,36 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'progress-bar',
+  selector: 'ion-progress-bar',
   template: `
-    <div class="progress-outer">
-      <div class="progress-inner" [style.width]="progress + '%'">
+    <div class="ion-progress-outer">
+      <div class="ion-progress-inner" [style.width]="progress + '%'" [style.background-color]="innerColor">
         {{progress}}%
       </div>
     </div>
-  `
+  `,
+  styles: [`
+    .ion-progress-outer {
+      width: 96%;
+      margin: 10px 2%;
+      padding: 3px;
+      text-align: center;
+      background-color: #f4f4f4;
+      border: 1px solid #dcdcdc;
+      color: #fff;
+      border-radius: 20px;
+    }
+    .ion-progress-inner {
+      min-width: 15%;
+      white-space: nowrap;
+      overflow: hidden;
+      padding: 5px;
+      border-radius: 20px;
+    }
+  `]
 })
 export class ProgressBarCmp {
-  @Input('progress') progress: number;
+  @Input() progress: number;
+  @Input() innerColor: string = '#387ef5';
   constructor() { }
 }
