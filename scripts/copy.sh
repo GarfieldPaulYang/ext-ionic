c832922f2a6550046be74f3f9ab85a9e5ec07678
squall <squall_lau@qq.com>
2017-05-22 16:17:28 +0800

    整理脚本

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-cp src/components/alpha-scroll/alpha-scroll.scss dist/components/alpha-scroll/
-
-cp src/components/baidu-map/baidu-map.scss dist/components/baidu-map/
-
-cp src/components/ribbon/ribbon.scss dist/components/ribbon/
-
-cp src/components/star-rating/star-rating.scss dist/components/star-rating/
-
+cp src/components/alpha-scroll/*.scss dist/components/alpha-scroll/
+cp src/components/baidu-map/*.scss dist/components/baidu-map/
+cp src/components/ribbon/*.scss dist/components/ribbon/
+cp src/components/star-rating/*.scss dist/components/star-rating/
 cp src/components/super-tabs/components/*.scss dist/components/super-tabs/components/
