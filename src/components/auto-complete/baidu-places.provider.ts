6b87774e4e299027d7f139b94951dd4ba8624b7e
squall <squall_lau@qq.com>
2017-07-14 15:54:11 +0800

    重构

--- src/components/auto-complete/baidu-places.provider.ts
+++ src/components/auto-complete/baidu-places.provider.ts
@@ -1,24 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Jsonp } from '@angular/http';
 import { AutoCompleteDataProvider } from './auto-complete';
 import { BaiduGeogProvider } from '../../providers/geog/geog';
 
 @Injectable()
 export class BaiduPlacesProvider implements AutoCompleteDataProvider {
-  constructor(private jsonp: Jsonp, private baiduGeogProvider: BaiduGeogProvider) { }
+  constructor(private baiduGeogProvider: BaiduGeogProvider) { }
 
   loadItems(params: any): Promise<any[]> {
-    params = {
-      ...this.getDefaultParams(),
-      ...params
-    };
-    const url = `http://api.map.baidu.com/place/v2/suggestion?q=${params.keyword}&region=${params.region}&location=${params.location}&city_limit=true&coord_type=${params.coordType}&ret_coordtype=${params.retCoordType}&output=json&ak=DmMSdcEILbFTUHs4QvlcV2G0`;
-    return this.jsonp.get(url, { params: { 'callback': 'JSONP_CALLBACK' } }).map((r => r.json())).toPromise().then(r => {
-      if (r.status !== 0) {
-        return Promise.reject(r.message);
-      }
-      return r.result;
-    }).catch(e => Promise.reject(e));
+    return this.baiduGeogProvider.suggestion(params);
   }
 
   init(params: any): Promise<any> {
@@ -27,20 +16,11 @@ export class BaiduPlacesProvider implements AutoCompleteDataProvider {
     }
 
     params = {
-      coordType: this.getDefaultParams().retCoordType,
+      coordType: params.retCoordType ? params.retCoordType : 'gcj02ll',
       ...params.initValue
     };
     return this.baiduGeogProvider.geocoder(params).then(data => {
       return data.addressComponent.street || data.formatted_address;
     });
-  }
-
-  private getDefaultParams(): any {
-    return {
-      region: '湖北省',
-      coordType: 'wgs84', // wgs84,gcj02,bd09
-      retCoordType: 'gcj02ll', // gcj02,bd09
-      location: '', // lat,lng
-    };
   }
 }
--- src/providers/geog/geog.ts
+++ src/providers/geog/geog.ts
@@ -8,6 +8,14 @@ export interface Geocoder extends GpsPoint {
   radius?: number;
 }
 
+export interface Suggestion {
+  keyword: string;
+  region?: string;
+  coordType?: 'wgs84' | 'wgs84' | 'gcj02' | 'bd09';
+  retCoordType?: 'gcj02ll' | 'gcj02' | 'bd09';
+  location?: string;
+}
+
 @Injectable()
 export class BaiduGeogProvider {
   private appKey: string = '12c17c924871a04f1dbac75e1824edb7';
@@ -23,6 +31,23 @@ export class BaiduGeogProvider {
       ...params
     };
     const url = `http://api.map.baidu.com/geocoder/v2/?coordtype=${params.coordType}&radius=${params.radius}&location=${params.lat},${params.lng}&output=json&pois=${params.pois}&ak=${this.appKey}`;
+    return this.jsonp.get(url, { params: { 'callback': 'JSONP_CALLBACK' } }).map((r => r.json())).toPromise().then(r => {
+      if (r.status !== 0) {
+        return Promise.reject(r.message);
+      }
+      return r.result;
+    }).catch(e => Promise.reject(e));
+  }
+
+  suggestion(params: Suggestion): Promise<any> {
+    params = {
+      region: '湖北省',
+      coordType: 'wgs84',
+      retCoordType: 'gcj02ll',
+      location: '',
+      ...params
+    };
+    const url = `http://api.map.baidu.com/place/v2/suggestion?q=${params.keyword}&region=${params.region}&location=${params.location}&city_limit=true&coord_type=${params.coordType}&ret_coordtype=${params.retCoordType}&output=json&ak=${this.appKey}`;
     return this.jsonp.get(url, { params: { 'callback': 'JSONP_CALLBACK' } }).map((r => r.json())).toPromise().then(r => {
       if (r.status !== 0) {
         return Promise.reject(r.message);
