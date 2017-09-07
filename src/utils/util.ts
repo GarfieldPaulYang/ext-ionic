14763014656fda0f6ff184910d050495be180922
am-wangjin <am-wangjin@gmail.com>
2017-09-07 15:39:55 +0800

    返回类型为number

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -125,5 +125,5 @@ export function division(dividend: number, divisor: number, toFixed?: number) {
   if (divisor === 0) {
     return 0;
   }
-  return (dividend / divisor).toFixed(toFixed ? toFixed : 2);
+  return Number((dividend / divisor).toFixed(toFixed ? toFixed : 2));
 }
