2b352ce66d368c952465519dc1096539b9d4f882
squall <squall_lau@qq.com>
2017-11-10 11:29:10 +0800

    去重复代码

--- src/utils/http/url-params-builder.ts
+++ src/utils/http/url-params-builder.ts
@@ -4,58 +4,39 @@ import * as _ from 'lodash';
 
 import { flattenObject } from '../../utils/util';
 
+const buildParams = (params: any, result: any): any => {
+  if (!_.isObject(params)) {
+    return null;
+  }
+
+  const paramsObj = flattenObject(params);
+  for (let key in paramsObj) {
+    let value = paramsObj[key];
+
+    if (_.isFunction(value)) {
+      continue;
+    }
+
+    if (_.isArray(value)) {
+      (<string[]>value).forEach(v => {
+        result.append(key, v);
+      });
+      continue;
+    }
+
+    result.set(key, <string>value);
+  }
+  return result;
+};
+
 export const HttpParamsBuilder = {
   build: (params: any): HttpParams => {
-    if (!_.isObject(params)) {
-      return null;
-    }
-
-    let paramsObj = flattenObject(params);
-    let result: HttpParams = new HttpParams();
-    for (let key in paramsObj) {
-      let value = paramsObj[key];
-
-      if (_.isFunction(value)) {
-        continue;
-      }
-
-      if (_.isArray(value)) {
-        (<string[]>value).forEach(v => {
-          result.append(key, v);
-        });
-        continue;
-      }
-
-      result.set(key, <string>value);
-    }
-    return result;
+    return buildParams(params, new HttpParams());
   },
 };
 
 export const URLParamsBuilder = {
   build: (params: any): URLSearchParams => {
-    if (!_.isObject(params)) {
-      return null;
-    }
-
-    let paramsObj = flattenObject(params);
-    let result: URLSearchParams = new URLSearchParams();
-    for (let key in paramsObj) {
-      let value = paramsObj[key];
-
-      if (_.isFunction(value)) {
-        continue;
-      }
-
-      if (_.isArray(value)) {
-        (<string[]>value).forEach(v => {
-          result.append(key, v);
-        });
-        continue;
-      }
-
-      result.set(key, <string>value);
-    }
-    return result;
+    return buildParams(params, new URLSearchParams());
   },
 };
