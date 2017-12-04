419482b5d7276133f9ef243567e047b16a16d6b8
squall <squall_lau@qq.com>
2017-12-04 15:00:03 +0800

    去掉遗留search属性

--- src/components/select/lazy-select.ts
+++ src/components/select/lazy-select.ts
@@ -66,7 +66,7 @@ export class LazySelect extends BaseInput<any> implements OnInit {
   ngOnInit(): void {
     this.http.get<Array<any>>(
       this.url,
-      { cache: true, cacheOnly: true, memCache: true, search: URLParamsBuilder.build(this.params), showLoading: false }
+      { cache: true, cacheOnly: true, memCache: true, params: URLParamsBuilder.build(this.params), showLoading: false }
     ).then((data: Array<any>) => {
       this.options = data;
     }).catch(e => {
